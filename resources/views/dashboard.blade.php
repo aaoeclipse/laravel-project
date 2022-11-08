5db702eefcfffe1d74b4bce877be1359f2e2210d
Santiago Paiz <santiago.paiz@gmail.com>
2022-11-08 13:42:55 +0100

    feat: :art: add blogs to dashboard (view only)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,62 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                <div class="p-6 bg-white border-b border-gray-200" style="padding: 2rem;">
+                <h2 class="font-bold text-3xl pb-4 text-center">Your Blogs</h2>
+                <div class="p-4 border-solid border flex justify-between items-center" style="padding: 1rem;">
+                    <div>
+                        <h3 class="font-bold text-3xl cursor-pointer">title</h3>
+                        <span class="text-gray-600">subtitle</span>
+                       </div>
+                       <div>
+                        <ul class="flex gap-4">
+                            <li class="text-center">
+                                <img src="" alt="likes">
+                                <span>10</span>
+                            </li>
+                            <li class="text-center">
+                                <img src="" alt="shares">
+                                <span>10</span>
+                            </li>
+                        </ul>
+                       </div>
+                   </div>
+                   <div class="p-4 border-solid border flex justify-between items-center" style="padding: 1rem;">
+                    <div>
+                        <h3 class="font-bold text-3xl cursor-pointer">title</h3>
+                        <span class="text-gray-600">subtitle</span>
+                       </div>
+                       <div>
+                        <ul class="flex gap-4">
+                            <li class="text-center">
+                                <img src="" alt="likes">
+                                <span>10</span>
+                            </li>
+                            <li class="text-center">
+                                <img src="" alt="shares">
+                                <span>10</span>
+                            </li>
+                        </ul>
+                       </div>
+                   </div>
+                   <div class="p-4 border-solid border flex justify-between items-center" style="padding: 1rem;">
+                    <div>
+                        <h3 class="font-bold text-3xl cursor-pointer">title</h3>
+                        <span class="text-gray-600">subtitle</span>
+                       </div>
+                       <div>
+                        <ul class="flex gap-4">
+                            <li class="text-center">
+                                <img src="" alt="likes">
+                                <span>10</span>
+                            </li>
+                            <li class="text-center">
+                                <img src="" alt="shares">
+                                <span>10</span>
+                            </li>
+                        </ul>
+                       </div>
+                   </div>
                 </div>
             </div>
         </div>
