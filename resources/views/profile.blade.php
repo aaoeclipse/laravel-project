d908d70145d3c4e0e1051fb844e7cae27ff62590
Santiago Paiz <santiago.paiz@gmail.com>
2022-11-08 15:24:37 +0100

    fix: :art: improve the output with curly braclets

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200" style="padding: 2rem;">
-                <h2 class="font-bold text-3xl pb-4 text-center">Blogs from <?=$user['username']?></h2>
+                <h2 class="font-bold text-3xl pb-4 text-center">Blogs from {{ $user->username}} </h2>
                 <div class="p-4 border-solid border flex justify-between items-center" style="padding: 1rem;">
                     <div>
                         <h3 class="font-bold text-3xl cursor-pointer">title</h3>
