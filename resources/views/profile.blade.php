6a43992b48d718d54ba939b6d15028b6a53923f2
Santiago Paiz <santiago.paiz@gmail.com>
2022-11-09 10:03:17 +0100

    feat: :card_file_box: add migration create blogs

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,7 +4,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200" style="padding: 2rem;">
                 <h2 class="font-bold text-3xl pb-4 text-center">Blogs from {{ $user->username}} </h2>
-                <div class="p-4 border-solid border flex justify-between items-center" style="padding: 1rem;">
+                (<div class="p-4 border-solid border flex justify-between items-center" style="padding: 1rem;">
                     <div>
                         <h3 class="font-bold text-3xl cursor-pointer">title</h3>
                         <span class="text-gray-600">subtitle</span>
@@ -21,43 +21,7 @@
                             </li>
                         </ul>
                        </div>
-                   </div>
-                   <div class="p-4 border-solid border flex justify-between items-center" style="padding: 1rem;">
-                    <div>
-                        <h3 class="font-bold text-3xl cursor-pointer">title</h3>
-                        <span class="text-gray-600">subtitle</span>
-                       </div>
-                       <div>
-                        <ul class="flex gap-4">
-                            <li class="text-center">
-                                <img src="" alt="likes">
-                                <span>10</span>
-                            </li>
-                            <li class="text-center">
-                                <img src="" alt="shares">
-                                <span>10</span>
-                            </li>
-                        </ul>
-                       </div>
-                   </div>
-                   <div class="p-4 border-solid border flex justify-between items-center" style="padding: 1rem;">
-                    <div>
-                        <h3 class="font-bold text-3xl cursor-pointer">title</h3>
-                        <span class="text-gray-600">subtitle</span>
-                       </div>
-                       <div>
-                        <ul class="flex gap-4">
-                            <li class="text-center">
-                                <img src="" alt="likes">
-                                <span>10</span>
-                            </li>
-                            <li class="text-center">
-                                <img src="" alt="shares">
-                                <span>10</span>
-                            </li>
-                        </ul>
-                       </div>
-                   </div>
+                   </div>)
                 </div>
             </div>
         </div>
