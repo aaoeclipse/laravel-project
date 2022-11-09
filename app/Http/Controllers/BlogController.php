4f3139f6c16527e064fe2a30861979fc08c27dfe
Santiago Paiz <santiago.paiz@gmail.com>
2022-11-09 11:34:08 +0100

    feat: :wrench: add blog routes and controller

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,7 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Blog;
+
 class BlogController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('blogcreation');
+    }
+
+    public function show($blog)
+    {
+        $blog = Blog::find($blog);
+
+        return view('blogcreation', ["blog" => $blog]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index']);
 
 Route::get('/profile/{user}', [\App\Http\Controllers\Profile::class, 'index']);
 
+Route::get('/blog',  [\App\Http\Controllers\BlogController::class, 'index']);
+Route::get('/blog/{blog}',  [\App\Http\Controllers\BlogController::class, 'show']);
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
