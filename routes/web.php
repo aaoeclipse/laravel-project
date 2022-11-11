8715e6fb8e281b1d7d24ebb82ef990047b5076f6
Santiago Paiz <santiago.paiz@gmail.com>
2022-11-11 11:19:20 +0100

    feat: :sparkles: add the blogs for the user dashboard

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index']);
 
-Route::get('/blog', [\App\Http\Controllers\BlogController::class, 'store']);
+Route::get('/blog', [\App\Http\Controllers\BlogController::class, 'store'])->name('blog.store');;
+Route::get('/blog/create', [\App\Http\Controllers\BlogController::class, 'create'])->name('blog.create');
 Route::get('/blog/{blog}', [\App\Http\Controllers\BlogController::class, 'show']);
 
 Route::get('/profile', [\App\Http\Controllers\Profile::class, 'store'])->middleware(['auth', 'verified'])->name('dashboard');
